8667ab5039ec55dd7234e3bc8c17697f6362b05b
igorStojanovic0 <cano19111@gmail.com>
2024-07-29 00:17:07 +0900

    env update

--- src/services/socket/config.js
+++ src/services/socket/config.js
@@ -1,9 +1,5 @@
 module.exports = {
 	port: 3002,
-	// notification relation
-	mainApi: 'http://localhost:3000',
-	
-	// relax relation
 	default_star: 100,
 	everyday_present: 50,
 	default_level: 1,
--- src/services/socket/config_sample.js
+++ src/services/socket/config_sample.js
@@ -1,9 +1,5 @@
 module.exports = {
 	port: 3002,
-	// notification relation
-	mainApi: 'http://localhost:3001/main',
-	
-	// relax relation
 	default_star: 100,
 	everyday_present: 50,
 	default_level: 1,
