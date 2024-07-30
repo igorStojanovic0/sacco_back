8d9f960f72eb2487fab3563f277ff295da0c82a8
igorStojanovic0 <cano19111@gmail.com>
2024-07-31 00:09:03 +0900

    getUser update

--- src/controller/auth.controllers.ts
+++ src/controller/auth.controllers.ts
@@ -116,16 +116,17 @@ export const signIn = asyncWrapper(async (req: Request, res: Response, next: Nex
 });
 
 export const getUserProfile = asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
-    // const authToken = req.get('Authorization');
+    const authToken = req.get('Authorization');
+    console.log("authToken", authToken);
     
     // if (!authToken?.split(' ')[1]) {
     //     return res.status(401).json({ message: "Access denied!" });
     // }
     
     const isValid = await isTokenValid(req);
-    // if (!isValid) {
-    //     return res.status(401).json({ message: "Access denied!" });
-    // }
+    if (!isValid) {
+        return res.status(401).json({ message: "Access denied!" });
+    }
 
     console.log("req.user", isValid);
     
